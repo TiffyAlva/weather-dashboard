3d9fb72d0f9804a711b9544cafa37b947b94eb62
TiffyAlva <TiffanyAlvarez54@gmail.com>
2022-08-19 11:03:16 -0400

    added README

---
+++ HOMEWORK/weather-dashboard/README.md
@@ -0,0 +1,13 @@
+# Weather-Dashboard
+
+### Deployed Link:
+
+
+## Module 6 Challenge
+- For traveler, it is presented with current and future conditions for that city.
+- Search history is supposed to be be added. Still working on it.
+- It shows a 5-day forecast that are display, the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity.
+
+![](./assets/image/Weather-dashboard-Page.jpg)
+
+
--- HOMEWORK/weather-dashboard/assets/script.js
+++ HOMEWORK/weather-dashboard/assets/script.js
@@ -4,6 +4,7 @@ var submitBtn = document.querySelector ("#submit");
 
 var cityName = "";
 
+// Current Weather
 function getCurrentWeather (lat, lon){
     var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + lon +"&units=imperial&appid=" + apiKey;
 
@@ -12,6 +13,11 @@ function getCurrentWeather (lat, lon){
     .then(data => { 
         console.log(data)
         displayCurrentWeather(data)
+        displayForecast(data, 0)
+        displayForecast(data, 1)
+        displayForecast(data, 2)
+        displayForecast(data, 3)
+        displayForecast(data, 4)
     })
 }
 
@@ -32,10 +38,13 @@ function getCoordinates () {
         getCurrentWeather(lat, lon)
     })
 }
-//*Current weather conditions for that city// 
+//Current weather conditions for that city// 
 function displayCurrentWeather (data) {
     var cityNameH2 = document.querySelector("#city-name");
     cityNameH2.textContent = cityName;
+
+    var dayH2 = document.querySelector("#today-date");
+    dayH2.textContent = dayjs().format("MMMM DD, YYYY");
 
     var tempH4 = document.querySelector("#temperature");
     tempH4.textContent = "Temp: " + data.current.temp;
@@ -50,8 +59,31 @@ function displayCurrentWeather (data) {
     var uvIndexH4 = document.querySelector("#uv-index");
     uvIndexH4.textContent = "UV Index: " + data.current.uvi
 
+    var icon = document.querySelector("#icon")
+    icon.src = "http://openweathermap.org/img/wn/"+ data.current.weather[0].icon +"@2x.png"
 }
 
+// 5-day forcast
+function displayForecast (data,index) {
+   
+    var dayH2 = document.querySelector("#date" + (index + 1));
+    dayH2.textContent = dayjs().add(index + 1, "day").format("MMMM DD, YYYY");
+
+    var tempH4 = document.querySelector("#temp" + (index + 1));
+    tempH4.textContent = "Temp: " + data.daily[index].temp.day;
+
+    var humidityH4 = document.querySelector("#hum" + (index + 1));
+    humidityH4.textContent = "Humidity: " + data.daily[index].humidity
+
+    var windSpeedH4 = document.querySelector
+    ("#wind" + (index + 1));
+    windSpeedH4.textContent = "Wind: " + data.daily[index].wind_speed
+
+    var icon = document.querySelector("#icon" + (index + 1))
+    icon.src = "http://openweathermap.org/img/wn/"+ data.daily[index].weather[0].icon +"@2x.png"
+}
+
+// Search Cities
 function searchCity (event) {
     event.preventDefault();
     cityName = cityInput.value;
@@ -60,8 +92,4 @@ function searchCity (event) {
 
 submitBtn.addEventListener("click", searchCity)
 
-// I was able to finish from line 46 to line 50
-// Add date next to city and add cloud icons
-// I need to display search history list
-// 5-day forecast that displays & shows current/future conditions and dates
-// FOLLOW Acceptance Criteria
+
